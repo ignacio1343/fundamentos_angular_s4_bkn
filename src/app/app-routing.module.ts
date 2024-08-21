ff03ee8892c1b14e235414a93bd180d7c50f6046
ignacuo <igna.bustos@duocuc.cl>
2024-08-21 12:56:51 -0400

    Rutas listas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+import { NopageComponent } from './pages/nopage/nopage.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+{
+  path:'',
+  component:HomeComponent
+},
+{
+  path:'home',
+  component:HomeComponent
+},
+{
+  path:'contacto',
+  component:ContactoComponent
+},
+{
+  path:'**',
+  component:NopageComponent
+},
+{
+  path:'ayuda',
+  component:AyudaComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
